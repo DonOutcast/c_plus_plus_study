2d7c52c5a9fb821993fc6398c89a7d8a91876d3a
Elizaveta890 <lizayakimova2006@gmail.com>
2023-06-08 20:17:39 +0300

    my

---
+++ Untitled-1.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <locale.h>
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    printf(" -  огда € добралс€ до них, мы вступили в агрессивные переговоры.\n");
+    printf(" - јгрессивные переговоры? Ёто как?\n");
+    printf(" - Ёто переговоры на cветовых мечах.\n");
+    return 0;
+}
