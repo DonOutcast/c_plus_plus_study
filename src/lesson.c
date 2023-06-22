c99c85f9d02c1f75cbaadcde73d25008187bf15c
DonOutcast <sham1996@yandex.ru>
2023-06-22 19:50:15 +0300

    fixed conflict

---
+++ src/lesson.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    switch (1) {
+    case 1:
+        printf("");
+        break;
+    
+    default:
+        printf("");
+        break;
+    }
+
+
+    return 0;
+}
