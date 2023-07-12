c30f8adad29ec1353ad33ca774924ca9df53be5b
DonOutcast <sham1996@yandex.ru>
2023-07-12 09:36:28 +0300

    text.txt

--- src/lesson.c
+++ src/lesson.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-
 int main() {
+int n=0;
+int number=0;
+int sumnumber=0;
+int numbercount=0;
+printf("Enter count:\n");
+scanf("%d",&n);
+printf("Enter numbers:\n");
+for(int i= 1;i <= n; i++) { 
+   scanf("%d",&number);
+   if(number%10==4) {
+       sumnumber += number;
+       numbercount++;
+}
 
-    switch (1) {
-    case 1:
-        printf("");
-        break;
-    
-    default:
-        printf("");
-        break;
-    }
+}
+printf("Count=%d\n",numbercount);
+printf("Sum=%d\n",sumnumber);
 
-
-    return 0;
+ return 0;
 }
