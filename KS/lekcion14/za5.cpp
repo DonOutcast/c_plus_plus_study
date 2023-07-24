04185371df43021984ac28eea0d1576c94d3b0b1
Kirill198619 <Flashpoint292@yandex.ru>
2023-07-24 16:53:46 +0300

    lekcion14

--- KS/lekcion14/za5.cpp
+++ KS/lekcion14/za5.cpp
@@ -30,10 +30,7 @@ int main()
    printf("vivod tablici \n");
     for( int i = 0 ; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            if(arr[i][j] == 0) {
-                printf("  ");
-            }
-            else
+        
             printf("%d ", arr[i][j]);
         }
         printf("\n");
