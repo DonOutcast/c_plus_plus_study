279909df6c76873fb33126e37191a6f37d71248c
Kirill198619 <Flashpoint292@yandex.ru>
2023-07-27 19:03:20 +0300

    lekcion15

---
+++ KS/lekcion151/za1.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n;
+    
+    scanf("%d", &n);
+
+    int *arr = (int*)malloc(n * sizeof(int));
+    int min = 0;;
+    int temp = 0;
+    for(int i = 0; i < n; i++)  {
+        scanf("%d", &arr[i]);
+    }
+    for(int i = 0; i < n; i++)  {
+        min = i;
+    
+        for(int j = i + 1; j < n; j++)  {
+            if(arr[j] < arr[temp])   {
+                min = j;
+            }
+        } 
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+      printf("\n");
+    for(int i = 0; i < n; i++)  {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
