a71932fe533b2fa22e2082fa237b56b1ccfe3fae
Elizaveta890 <lizayakimova2006@gmail.com>
2023-09-14 19:35:58 +0300

    Hw28

--- liza/hw28/hw28_1.cpp
+++ liza/hw28/hw28_1.cpp
@@ -10,12 +10,12 @@ int main(){
     try
     {
         {
-            for (int i = 0; i < symbols.size(); i++){
+            for (int i = 0; i < username.size(); i++){
                 if (username.find(symbols[i]) != -1){
                     find = 0;
                 }
             }
-            for (int i = 0; i < symbols.size(); i++){
+            for (int i = 0; i < username.size(); i++){
                 if (username.find(numbers[i]) == 0){
                     throw "First character error";
                 }
@@ -23,7 +23,7 @@ int main(){
             if (find == 0){
                 throw "Wrong character error";
             }
-            for (int i = 0; i < symbols.size(); i++){
+            for (int i = 0; i < username.size(); i++){
                 if (username.find(numbers[i]) != 0 && find != 0){
                     std::cout << "OK" << std::endl;
                     break;
