ad9152d36d62fffdf1990a16b1cc1f34a90d2251
Elizaveta890 <lizayakimova2006@gmail.com>
2023-09-28 10:35:56 +0300

    MyForm

---
+++ liza/file/hello.cpp
@@ -0,0 +1,17 @@
+#include "w29.hpp"
+
+namespace my{
+    
+    int MathematicalFunctions::plus(int a, int b){
+        return a + b;
+    }
+    int MathematicalFunctions::minus(int a, int b){
+        return a - b;
+    }
+    int MathematicalFunctions::multiply(int a, int b){
+        return a * b;
+    }
+    int MathematicalFunctions::divide(int a, int b){
+        return a / b;
+    }
+}
---
+++ liza/file/w29.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+namespace my{
+    class MathematicalFunctions{
+        public:
+            MathematicalFunctions(){};
+            int plus(int a, int b);
+            int minus(int a, int b);
+            int multiply(int a, int b);
+            int divide(int a, int b);
+    };
+}
